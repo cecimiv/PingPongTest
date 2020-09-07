d2f65baa37bb4d1aee24c84aadd1d8650ba92b4c
cecimiv <ceciversen@gmail.com>
2020-09-07 10:09:52 +0200

    Second test:)

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -5,13 +5,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
 @Test
-    void shouldConvertToOne(){
+    void shouldConvertToOne() {
     assertEquals("I", toRoman(1));
 
 }
+    @Test
+    void shouldConvertToTwo(){
+    assertEquals("II", toRoman(2));
 
-    private String toRoman(int i) {
-        return null;
+}
+
+
+
+    private String toRoman(int number) {
+        String result = "";
+        while(number >= 10){
+            result += "X";
+            number -= 10;
+        }
+        if(number >= 9){
+            result += "IX";
+            number -= 9;
+        }
+        if(number >= 5){
+            result += "V";
+            number -= 5;
+        }
+        if(number >= 4){
+            result += "IV";
+            number -= 4;
+        }
+        for (int i = 0; i < number; i++) {
+            result += "I";
+        }
+
+        return result;
     }
 
 
