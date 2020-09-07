ea7ee0ef71a5625b6c576d97be4ffbbe8b40b0fc
hannaelarsen <hanna.elise.larsen@gmail.com>
2020-09-07 10:06:35 +0200

    første test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,10 +1,18 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class MainTest {
 @Test
     void shouldConvertToOne(){
+    assertEquals("I", toRoman(1));
 
 }
+
+    private String toRoman(int i) {
+        return null;
+    }
 
 
 }
